afb8d88ee1c6d5685ca89bab72dbfba9e792c3ed
Vitor Alves <vitormanoel00710@gmail.com>
2022-04-11 11:30:24 -0300

    Added project api person with all http verbs

---
+++ 03-restful-asp-net-Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/IPersonService.cs
@@ -0,0 +1,19 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person FindByID(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Person person);
+
+        void Delete(long id);
+
+        
+    }
+}
