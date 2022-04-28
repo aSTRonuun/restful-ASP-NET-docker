80a8da660eb48e79499ac9e859aee342fd4537b3
Vitor Alves <vitormanoel00710@gmail.com>
2022-04-28 18:26:09 -0300

    Added authentication configuration classes and intarfaces

--- 03-restful-asp-net-Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/UserRepository.cs
+++ 03-restful-asp-net-Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/UserRepository.cs
@@ -27,5 +27,27 @@ namespace RestWithASPNETUdemy.Repository
             Byte[] hashedBytes = algorithm.ComputeHash(inputBytes);
             return BitConverter.ToString(hashedBytes);
         }
+
+        public User RefreshUserInfo(User user)
+        {
+            if (_context.Users.Any(u => user.Id.Equals(user.Id))) return null;
+
+            var result = _context.Users.SingleOrDefault(p => p.Id == user.Id);
+            Console.WriteLine(result);
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(user);
+                    _context.SaveChanges();
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return result;
+        }
     }
 }
